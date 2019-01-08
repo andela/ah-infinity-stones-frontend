ed12060be343940b566f56733a7d94b95079e34f
Leo <leonard.njura@gmail.com>
2019-01-08 11:47:37 +0300

    chore(sass): Setup sass preprocessor -  setup sass style compiler -  app can compile .sass or .scss into .css stylesheets
    [Delivers #161966868]

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/fonts/font-awesome.min.css';
+import './assets/fonts/materialdesignicons.min.css';
+import './assets/css/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
