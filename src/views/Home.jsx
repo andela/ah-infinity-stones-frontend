6690f5385f9bf3b2c763bed1c0b1a88ad4d52a0f
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-01-09 17:06:19 +0300

    Chore(React-router-v4): Setup react router version 4
    - Install and configure the router creating a dummy route for login and the "/" page to test the configuration
    - Create a components and views file structure where view pages are in the views folder but individual components are in the components folder.
    
    [Delivers #161966867]

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="">
+        <h4 className="center">Home</h4>
+        <p>Test configuration</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
