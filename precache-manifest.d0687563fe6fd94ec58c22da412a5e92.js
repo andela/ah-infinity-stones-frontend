67f92bbd103de7f7549e71544ca6bd23b4f0aede
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-15 08:29:03 +0000

    Deploy andela/ah-infinity-stones-frontend to github.com/andela/ah-infinity-stones-frontend.git:gh-pages

---
+++ precache-manifest.d0687563fe6fd94ec58c22da412a5e92.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ae831afb1acbf6f7e70",
+    "url": "/static/css/main.5c9f3821.chunk.css"
+  },
+  {
+    "revision": "2ae831afb1acbf6f7e70",
+    "url": "/static/js/main.2ae831af.chunk.js"
+  },
+  {
+    "revision": "cb99cdbffb94f6579818",
+    "url": "/static/css/1.9060283f.chunk.css"
+  },
+  {
+    "revision": "cb99cdbffb94f6579818",
+    "url": "/static/js/1.cb99cdbf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "737cdf1d697638a09d934656421bc92c",
+    "url": "/static/media/loading_spinner.737cdf1d.gif"
+  },
+  {
+    "revision": "f64cc8b53eb979a50ff7337857217a07",
+    "url": "/static/media/logo.f64cc8b5.png"
+  },
+  {
+    "revision": "2eabffd17a7871f75e41b488f00ad6c4",
+    "url": "/static/media/write_a_story.2eabffd1.jpeg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4b1359677a76d07aa0526d2fddbd77b7",
+    "url": "/static/media/materialdesignicons-webfont.4b135967.woff2"
+  },
+  {
+    "revision": "6473d7d5a01bb3f8fccd5bbeaf2be312",
+    "url": "/static/media/materialdesignicons-webfont.6473d7d5.woff"
+  },
+  {
+    "revision": "1eccbc4c41d49fd81840aef3eaabe862",
+    "url": "/static/media/materialdesignicons-webfont.1eccbc4c.ttf"
+  },
+  {
+    "revision": "3efec83af3e00d224094f2889c32edb6",
+    "url": "/static/media/materialdesignicons-webfont.3efec83a.eot"
+  },
+  {
+    "revision": "5dfd79fe0d7e27715ddfbbc7d4ed8dd1",
+    "url": "/static/media/materialdesignicons-webfont.5dfd79fe.svg"
+  },
+  {
+    "revision": "66dc5b48e262ea5b2a53de8a031679e0",
+    "url": "/index.html"
+  }
+];
